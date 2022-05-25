ea6d6d6f7c54f789082674081d3dbbb55dccf139
Raidienaty <jcanducci@gmail.com>
2022-05-25 10:20:26 -0400

    Reads excel doc into dataframe

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from itertools import count
 import pandas
 import os
+
+COURSE_GRID_TEMPLATE = 'Course Grid UPDATED.xlsx'
 
 #path =  os.path.realpath(__file__)
 directoryList = os.listdir('.')
@@ -10,22 +11,31 @@ print('Which file would you like to use?')
 excelDocs = []
 
 for item in directoryList:
-    if item.find('Course Grid UPDATED.xlsx') != -1:
+    if item.find(COURSE_GRID_TEMPLATE) != -1:
         continue
     elif item.find('.xlsx') != -1:
         excelDocs.append(item)
 
+selectedDoc = ''
 
 if len(excelDocs) < 1:
     print('Please move the course report to my folder!')
     exit()
 elif len(excelDocs) > 1:
     print('Which of the following files would you like to use?')
-    for item in directoryList:
-        if item.count('.xlsx'):
-            print(item)
+    for item in excelDocs:
+        print(item)
 else:
-    print('Using ')
-    for item in directoryList: 
-        if item.count('*.xlsx'):
-            print(item)
+    print('Using: ')
+    for item in excelDocs: 
+        print(item)
+
+    selectedDoc = excelDocs[0]
+
+courseTemplate = pandas.read_excel(COURSE_GRID_TEMPLATE)
+courseReport = pandas.read_excel(selectedDoc)
+
+print(courseReport.head(10))
+print()
+print(courseTemplate.head(10))
+
