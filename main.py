0e406b036d86adc455621c7c498824ac1cdc1113
Raidienaty <jcanducci@gmail.com>
2022-05-31 10:29:25 -0400

    Attempting to fix the course bleed through issue

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pandas
 import os
 import re
+import xlsxwriter
 
 COURSE_GRID_TEMPLATE = 'Course Grid UPDATED.xlsx'
 COURSE_GRID_POSITIONS = [
@@ -10,6 +11,13 @@ COURSE_GRID_POSITIONS = [
     {'13': 0, '14': 1, '5': 2, '7': 3, '9': 4, '11': 5, '21': 6, '23': 7},
     {'2': 0, '4': 1, '6': 2, '8': 3, '10': 4, '12': 5}
 ]
+
+def get_col_widths(dataframe):
+    # First we find the maximum length of the index column   
+    idx_max = max([len(str(s)) for s in dataframe.index.values] + [len(str(dataframe.index.name))])
+    # Then, we concatenate this to the max of the lengths of column name and its values for each column, left to right
+    return [idx_max] + [max([len(str(s)) for s in dataframe[col].values] + [len(col)]) for col in dataframe.columns]
+
 
 #path =  os.path.realpath(__file__)
 directoryList = os.listdir('.')
@@ -94,8 +102,18 @@ for major in courses:
 
                 courseSchedules[major][day][position] += '\n' + str(course[1]['Section Name']) + '-' +  str(course[1]['Instructor Last Name']) + '-' + str(course[1]['Meeting Building']) + '-' + str(course[1]['Meeting Room'])
 
-    courseSchedules[major].to_excel('Schedules/' + major + '_course_schedule.xlsx')
+    # writer = pandas.ExcelWriter('Schedules/' + major + '_course_schedule.xlsx', engine='xlsxwriter')
+    # courseSchedules[major].to_excel(writer, sheet_name=major, index=False)
 
+    # workbook = writer.book
+    # worksheet = writer.sheets[major]
+
+    # writer.save()
+
+    courseSchedules[major].to_excel('Schedules/' + major + '_course_schedule.xlsx', sheet_name=major, index=False)
+
+    # format = workbook.add_format({'text_wrap': True, 'align': 'top left'})
+    # worksheet.set_column(0, len(courseSchedules[major].columns) - 1, cell_format=format)
 
     # courseSchedules[major].loc[]
 
